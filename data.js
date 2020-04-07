88d939a31c59565aeb8511c654a712f6ecf0a792
Nurhamid <40646329+nurhamid05@users.noreply.github.com>
2020-04-07 14:25:47 +0700

    Update data.js

--- data.js
+++ data.js
@@ -181,7 +181,7 @@ var sekernan = {
   "features": [
     {
       "type": "Feature",
-      "properties": {"Kecamatan":"Sekernan","ODP": "7","PDP": "-"},
+      "properties": {"Kecamatan":"Sekernan","ODP": "7","PDP": "0"},
       "geometry": {
         "type": "Point",
         "coordinates": [
